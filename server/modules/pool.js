fffce73514c3dc0149c751db634252cc64e92f1d
Andrew-R-Lawler <61293731+Andrew-R-Lawler@users.noreply.github.com>
2022-09-22 14:53:35 -0500

    server posts user registration data correctly

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,28 +1,30 @@
 const pg = require('pg');
 const url = require('url');
+require('dotenv').config()
 
 let config = {};
 
 if (process.env.DATABASE_URL) {
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
+//   const params = url.parse(process.env.DATABASE_URL);
+//   const auth = params.auth.split(':');
 
   // This configuration connects to the database in AWS called ' pretslonboardingapptest '
 
   config = {
     user: 'postgres',
-    password: process.env.DATABASE_PASSWORD,
+    password: 'postgres',
     host: 'localhost',
     port: 5432,
     database: 'template-shop-users',
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
-}
+};
 // this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
+
 
 // the pool with emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
